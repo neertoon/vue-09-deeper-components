019a5860c16776da6a7f967f62b444c87bfc5e2e
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2025-09-25 16:05:44 +0200

    Przykład (problem) pod wprowadzenie kolejnego rozwiązania...

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,22 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+
+</script>
+
+<style>
+dialog {
+  margin: 0;
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  background-color: white;
+  padding: 1rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Add</button>
+    <error-alert v-if="inputIsInvalid" >
+      <h2>Input invalid</h2>
+      <p>Please enter a valid goal!</p>
+      <button @click="inputIsInvalid = false">OK</button>
+    </error-alert>
   </div>
 </template>
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
+    }
+  }
+}
+</script>
