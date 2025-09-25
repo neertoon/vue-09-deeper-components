e0ad02e50327bf5b0ae21097a9545e63d1668455
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2025-09-25 16:09:29 +0200

    Teleport - przenieś komponent w drzewie DOM w inne miejsce

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,11 +3,13 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Add</button>
+    <teleport to="body">
     <error-alert v-if="inputIsInvalid" >
       <h2>Input invalid</h2>
       <p>Please enter a valid goal!</p>
       <button @click="inputIsInvalid = false">OK</button>
     </error-alert>
+    </teleport>
   </div>
 </template>
 <script>
